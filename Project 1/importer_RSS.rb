a1c52a5ca3a512bb96e238789b6bdeac8a8460be
Song Xue <sxue1@student.unimelb.edu.au>
2015-08-20 12:13:23 +1000

    Finish the first draft of the Project 1. Need to test and check with Raymond and Rahul.

--- Project 1/importer_RSS.rb
+++ Project 1/importer_RSS.rb
@@ -22,7 +22,13 @@ class Importer_RSS < News::Importer
     @feed.items.each do |item|
     #set different items to article object
     #how to get author, image ?
-      article = News::Article.new 'Blank', item.title.delete(','), item.description.to_s.delete(',','\s'), 'Blank', item.link, item.pubDate.to_s.delete(',')
+      article = News::Article_AGE.new 'Blank',
+                                     item.title.delete(','),
+                                     item.description.to_s.delete(',','\s'),
+                                     'Blank',
+                                     self.source_name,
+                                     item.pubDate.to_s.delete(','),
+                                     item.link
       @articles << article
     end
     @articles.each do |article|
